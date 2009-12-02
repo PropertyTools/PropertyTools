a3b008c3912f2bc9913929990ebc3dc57305934f
Oystein Bjorke <oystein.bjorke@gmail.com>
2009-12-02 21:38:07 +0000

    Added Slidable attribute (adds slider control to properties that can be converted to double)

---
+++ Source/PropertyEditor/Converters/ToDoubleConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace OpenControls
+{
+    /// <summary>
+    /// Usage 'Converter={local:ToDoubleConverter}'
+    /// </summary>
+    [ValueConversion(typeof(object), typeof(double))]
+    public class ToDoubleConverter : SelfProvider, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return 0;
+            if (value is double)
+                return (double) value;
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+          //  if (targetType==typeof(double))
+                return value;
+//            return Binding.DoNothing;
+        }
+    }
+}

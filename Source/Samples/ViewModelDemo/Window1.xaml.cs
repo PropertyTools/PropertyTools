057ec1387e3ccb438b70969918b6e1b3b013599d
Oystein Bjorke <oystein.bjorke@gmail.com>
2009-12-11 16:41:56 +0000

    Updated ViewModel example

--- Source/Samples/ViewModelDemo/Window1.xaml.cs
+++ Source/Samples/ViewModelDemo/Window1.xaml.cs
@@ -22,8 +22,15 @@ namespace ViewModelDemo
         public Window1()
         {
             InitializeComponent();
-            var p = new Person() {Name = "John Johnson", Age = 59};
-            var pvm = new PersonViewModel(p);
+            var p1 = new Person() { Name = "John Johnson", Age = 59, Height = 1.78 };
+            var p2 = new Person() { Name = "Bill Williams", Age = 59, Height = 1.72 };
+            
+            var people = new List<Person>();
+            people.Add(p1);
+            people.Add(p2);
+//            editor1.SelectedObjects = people;
+
+            var pvm = new PersonViewModel(p1);
             editor1.SelectedObject = pvm;
             group1.DataContext = pvm;
         }

74f652db206ae963dc24a289b2551d5fb022dfa8
Oystein Bjorke <oystein.bjorke@gmail.com>
2010-09-22 14:20:29 +0000

    Added a PropertyStateProvider

--- Source/Examples/ViewModelDemo/Window1.xaml.cs
+++ Source/Examples/ViewModelDemo/Window1.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows;
+using PropertyEditorLibrary;
 
 namespace ViewModelDemo
 {
@@ -8,19 +10,22 @@ namespace ViewModelDemo
     /// </summary>
     public partial class Window1 : Window
     {
+        public PersonViewModel Person { get; private set; }
+        public IPropertyStateProvider PropertyStateProvider { get; private set; }
+
         public Window1()
         {
             InitializeComponent();
+            PropertyStateProvider = new PropertyStateProvider();
             var p1 = new Person() { Name = "John Johnson", Age = 59, Height = 1.78 };
             var p2 = new Person() { Name = "Bill Williams", Age = 59, Height = 1.72 };
-            
+
             var people = new List<Person>();
             people.Add(p1);
             people.Add(p2);
 
-            var pvm = new PersonViewModel(p1);
-            editor1.SelectedObject = pvm;
-            group1.DataContext = pvm;
+            Person = new PersonViewModel(p1);
+            DataContext = this;
         }
     }
 }

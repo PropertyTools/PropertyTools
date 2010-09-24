832f60c2decbe538341f06187f71f475ea2f2f56
Oystein Bjorke <oystein.bjorke@gmail.com>
2010-09-24 06:02:55 +0000

    Updated templates for error/warning - now using borders around the editing controls

---
+++ Source/PropertyEditor/Converters/NullToBoolConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PropertyEditorLibrary
+{
+    /// <summary>
+    /// null to bool valueconverter
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(object))]
+    public class NullToBoolConverter : SelfProvider, IValueConverter
+    {
+        public bool NullValue { get; set; }
+
+        public NullToBoolConverter()
+        {
+            NullValue = true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return NullValue;
+            return !NullValue;
+        }
+
+    }
+}

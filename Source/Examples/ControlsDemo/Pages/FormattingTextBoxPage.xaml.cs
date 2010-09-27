c22d2d5dd9adf8595014c411b4a738829b659493
Oystein Bjorke <oystein.bjorke@gmail.com>
2010-09-27 21:30:49 +0000

    Added FormattingTextBox control - testing it with 'slidable' properties

---
+++ Source/Examples/ControlsDemo/Pages/FormattingTextBoxPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ControlsDemo.Pages
+{
+    /// <summary>
+    /// Interaction logic for FormattingTextBoxPage.xaml
+    /// </summary>
+    public partial class FormattingTextBoxPage : Page
+    {
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof (double), typeof (MainWindow), new UIPropertyMetadata(Math.PI));
+
+        public static readonly DependencyProperty TimeProperty =
+            DependencyProperty.Register("Time", typeof (DateTime), typeof (MainWindow),
+                                        new UIPropertyMetadata(DateTime.Now));
+
+        public static readonly DependencyProperty StringProperty =
+            DependencyProperty.Register("String", typeof (string), typeof (MainWindow), new UIPropertyMetadata("John"));
+
+        public FormattingTextBoxPage()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+
+        public double Value
+        {
+            get { return (double) GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public DateTime Time
+        {
+            get { return (DateTime) GetValue(TimeProperty); }
+            set { SetValue(TimeProperty, value); }
+        }
+
+        public string String
+        {
+            get { return (string) GetValue(StringProperty); }
+            set { SetValue(StringProperty, value); }
+        }
+    }
+}

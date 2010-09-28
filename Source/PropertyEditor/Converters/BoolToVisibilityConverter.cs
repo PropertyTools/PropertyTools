9216c2f40f75e69ba78f49ba98e2d752f8995034
Oystein Bjorke <oystein.bjorke@gmail.com>
2010-09-28 05:53:53 +0000

    Corrected bug in BoolToVisibilityConverter, Issue #5602

--- Source/PropertyEditor/Converters/BoolToVisibilityConverter.cs
+++ Source/PropertyEditor/Converters/BoolToVisibilityConverter.cs
@@ -7,15 +7,21 @@ namespace PropertyEditorLibrary
     /// <summary>
     /// Bool to Visibility value converter
     /// </summary>
-    [ValueConversion(typeof(Visibility), typeof(bool))]
+    [ValueConversion(typeof(bool), typeof(Visibility))]
     public class BoolToVisibilityConverter :IValueConverter
     {
-        public bool InvertVisibility
-        { get; set; }
+        public bool InvertVisibility { get; set; }
+        public Visibility NotVisibleValue { get; set; }
+
+        public BoolToVisibilityConverter()
+        {
+            InvertVisibility = false;
+            NotVisibleValue = Visibility.Collapsed;
+        }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            return null;
+            return ((value is Visibility) && (((Visibility)value) == Visibility.Visible)) ? !InvertVisibility : InvertVisibility;
         }
 
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
@@ -23,19 +29,22 @@ namespace PropertyEditorLibrary
             if (value == null)
                 return Visibility.Visible;
 
-            bool visible = (bool)value;
+            bool visible=true;
+            if (value is bool)
+            {
+                visible = (bool)value;
+            }
+            else if (value is bool?)
+            {
+                var nullable = (bool?)value;
+                visible = nullable.HasValue ? nullable.Value : false;
+            }
+
 
             if (InvertVisibility)
                 visible = !visible;
 
-            if (visible)
-            {
-                return Visibility.Visible;
-            }
-            else
-            {
-                return Visibility.Hidden;
-            }
+            return visible ? Visibility.Visible : NotVisibleValue;
         }
 
     }
--- Source/PropertyEditorTests/ConverterTests.cs
+++ Source/PropertyEditorTests/ConverterTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Windows;
+using NUnit.Framework;
+using PropertyEditorLibrary;
 
 namespace PropertyEditorTests
 {
@@ -11,5 +13,36 @@ namespace PropertyEditorTests
             //todo
         }
 
+        [Test]
+        public void BoolToVisibility_NotInverted_ReturnsCorrectResult()
+        {
+            var btv = new BoolToVisibilityConverter();
+            Assert.AreEqual(Visibility.Visible,btv.Convert(true, typeof (Visibility), null, null));
+            Assert.AreEqual(Visibility.Collapsed, btv.Convert(false, typeof(Visibility), null, null));
+            Assert.AreEqual(true, btv.ConvertBack(Visibility.Visible, typeof(bool), null, null));
+            Assert.AreEqual(false, btv.ConvertBack(Visibility.Hidden, typeof(bool), null, null));
+            Assert.AreEqual(false, btv.ConvertBack(Visibility.Collapsed, typeof(bool), null, null));
+
+            btv.NotVisibleValue = Visibility.Hidden;
+            Assert.AreEqual(Visibility.Hidden, btv.Convert(false, typeof(Visibility), null, null));
+            Assert.AreEqual(true, btv.ConvertBack(Visibility.Visible, typeof(bool), null, null));
+            Assert.AreEqual(false, btv.ConvertBack(Visibility.Hidden, typeof(bool), null, null));
+            Assert.AreEqual(false, btv.ConvertBack(Visibility.Collapsed, typeof(bool), null, null));
+        }
+
+        [Test]
+        public void BoolToVisibility_Inverted_ReturnsCorrectResult()
+        {
+            var btv = new BoolToVisibilityConverter() { InvertVisibility = true };
+            Assert.AreEqual(Visibility.Visible, btv.Convert(false, typeof(Visibility), null, null));
+            Assert.AreEqual(Visibility.Collapsed, btv.Convert(true, typeof(Visibility), null, null));
+            Assert.AreEqual(false, btv.ConvertBack(Visibility.Visible, typeof(bool), null, null));
+            Assert.AreEqual(true, btv.ConvertBack(Visibility.Hidden, typeof(bool), null, null));
+
+            btv.NotVisibleValue = Visibility.Hidden;
+            Assert.AreEqual(Visibility.Hidden, btv.Convert(true, typeof(Visibility), null, null));
+            Assert.AreEqual(false, btv.ConvertBack(Visibility.Visible, typeof(bool), null, null));
+            Assert.AreEqual(true, btv.ConvertBack(Visibility.Hidden, typeof(bool), null, null));
+        }
     }
 }

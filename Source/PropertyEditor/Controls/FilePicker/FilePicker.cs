0ee33d0ec8949596622d7649ecac08c943a061fd
Oystein Bjorke <oystein.bjorke@gmail.com>
2010-10-04 21:27:33 +0000

    FilePicker/DirectoryPicker: Corrected binding bug

--- Source/PropertyEditor/Controls/FilePicker/FilePicker.cs
+++ Source/PropertyEditor/Controls/FilePicker/FilePicker.cs
@@ -1,12 +1,7 @@
-﻿using System.Collections.ObjectModel;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
-using System.ComponentModel;
-using System;
-using System.Reflection;
-using System.Windows.Threading;
 using System.Windows.Input;
+using Microsoft.Win32;
 
 namespace PropertyEditorLibrary
 {
@@ -15,45 +10,50 @@ namespace PropertyEditorLibrary
     /// </summary>
     public class FilePicker : Control
     {
-        static FilePicker()
-        {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(FilePicker), new FrameworkPropertyMetadata(typeof(FilePicker)));
-        }
-
-        public string FilePath
-        {
-            get { return (string)GetValue(FilePathProperty); }
-            set { SetValue(FilePathProperty, value); }
-        }
-
         public static readonly DependencyProperty FilePathProperty =
-            DependencyProperty.Register("FilePath", typeof(string), typeof(FilePicker), new UIPropertyMetadata(null));
-
-        public string Filter
-        {
-            get { return (string)GetValue(FilterProperty); }
-            set { SetValue(FilterProperty, value); }
-        }
+            DependencyProperty.Register("FilePath", typeof (string), typeof (FilePicker),
+                                        new FrameworkPropertyMetadata(null,
+                                                                      FrameworkPropertyMetadataOptions.
+                                                                          BindsTwoWayByDefault));
 
         public static readonly DependencyProperty FilterProperty =
-            DependencyProperty.Register("Filter", typeof(string), typeof(FilePicker), new UIPropertyMetadata(null));
-
-        public string DefaultExtension
-        {
-            get { return (string)GetValue(DefaultExtensionProperty); }
-            set { SetValue(DefaultExtensionProperty, value); }
-        }
+            DependencyProperty.Register("Filter", typeof (string), typeof (FilePicker), new UIPropertyMetadata(null));
 
         public static readonly DependencyProperty DefaultExtensionProperty =
-            DependencyProperty.Register("DefaultExtension", typeof(string), typeof(FilePicker), new UIPropertyMetadata(null));
+            DependencyProperty.Register("DefaultExtension", typeof (string), typeof (FilePicker),
+                                        new UIPropertyMetadata(null));
 
-        public ICommand BrowseCommand { get; set; }
-        public IFileDialog FileDialog { get; set; }
+        static FilePicker()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof (FilePicker),
+                                                     new FrameworkPropertyMetadata(typeof (FilePicker)));
+        }
 
         public FilePicker()
         {
             BrowseCommand = new DelegateCommand(Browse);
         }
+
+        public string FilePath
+        {
+            get { return (string) GetValue(FilePathProperty); }
+            set { SetValue(FilePathProperty, value); }
+        }
+
+        public string Filter
+        {
+            get { return (string) GetValue(FilterProperty); }
+            set { SetValue(FilterProperty, value); }
+        }
+
+        public string DefaultExtension
+        {
+            get { return (string) GetValue(DefaultExtensionProperty); }
+            set { SetValue(DefaultExtensionProperty, value); }
+        }
+
+        public ICommand BrowseCommand { get; set; }
+        public IFileDialog FileDialog { get; set; }
 
         private void Browse()
         {
@@ -68,7 +68,7 @@ namespace PropertyEditorLibrary
             else
             {
                 // use default win32 dialog
-                var d = new Microsoft.Win32.OpenFileDialog();
+                var d = new OpenFileDialog();
                 d.FileName = FilePath;
                 d.Filter = Filter;
                 d.DefaultExt = DefaultExtension;
@@ -87,5 +87,4 @@ namespace PropertyEditorLibrary
         string FileName { get; set; }
         bool Show();
     }
-
 }

fa80e174a9276c378408346b420236e651fc30fe
Oystein Bjorke <oystein.bjorke@gmail.com>
2010-11-23 21:13:25 +0000

    Added performance test application (unfinished)

---
+++ Source/Examples/PerformanceTest/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace PerformanceTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Test1_Click(object sender, RoutedEventArgs e)
+        {
+            propertyEditor1.SelectedObject = new TestClass1();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            propertyEditor1.SelectedObject = null;
+        }
+    }
+}

302df5db2da01474cb150b8ac1c718944c9d4350
objo <objo@localhost>
2010-12-05 20:27:19 +0000

    Added new controls: DockPanelSplitter and EnumMenuItem

---
+++ Source/Examples/ControlsDemo/Pages/DockPanelSplitterPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ControlsDemo
+{
+    /// <summary>
+    /// Interaction logic for DockPanelSplitterPage.xaml
+    /// </summary>
+    public partial class DockPanelSplitterPage : Page
+    {
+        public DockPanelSplitterPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

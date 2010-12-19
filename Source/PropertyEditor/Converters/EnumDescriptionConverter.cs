75edc87aadbee556cec2ff0e205ec88ab5af145a
objo <objo@localhost>
2010-12-19 12:24:39 +0000

    Refactored EnumDescriptionConverter to use LINQ.

--- Source/PropertyEditor/Converters/EnumDescriptionConverter.cs
+++ Source/PropertyEditor/Converters/EnumDescriptionConverter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.ComponentModel;
 using System.Reflection;
 using System.Windows.Data;
@@ -16,25 +18,9 @@ namespace PropertyEditorLibrary
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             var enumType = value.GetType();
-            foreach (FieldInfo field in enumType.GetFields(BindingFlags.Static | BindingFlags.GetField | BindingFlags.Public))
-            {
-                var fieldValue = field.GetValue(null);
-
-                if (!fieldValue.Equals(value))
-                    continue;
-
-                foreach (var attrib in field.GetCustomAttributes(true))
-                {
-                    var da = attrib as DescriptionAttribute;
-                    if (da != null)
-                    {
-                        return da.Description;
-                    }
-                }
-                return value.ToString();
-            }
-
-            return value.ToString();
+            var field = enumType.GetFields(BindingFlags.Static | BindingFlags.GetField | BindingFlags.Public).First(f => f.GetValue(null).Equals(value));
+            var descriptionAttribute = field.GetCustomAttributes<DescriptionAttribute>(true).FirstOrDefault();
+            return descriptionAttribute != null ? descriptionAttribute.Description : value;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
@@ -43,5 +29,13 @@ namespace PropertyEditorLibrary
         }
 
         #endregion
+    }
+
+    public static class ReflectionExtensions
+    {
+        public static IEnumerable<T> GetCustomAttributes<T>(this FieldInfo fieldInfo, bool inherit)
+        {
+            return fieldInfo.GetCustomAttributes(typeof(T), inherit).Cast<T>();
+        }
     }
 }

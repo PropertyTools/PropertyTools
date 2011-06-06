3d3c3c2a1670074bbfc3b9c2a87ea6555abe9126
PerMalmberg <PerMalmberg@localhost>
2011-06-06 21:28:36 +0000

    Fields in enums now honor the Browsable attribute, making it possible to hide certain values from being user-selectable. This is especially usefull when the enums start/ends with 'invalid' values, such as:
    public enum MyEnum {
        [Browsable(false)
        START = 0,
        Actual,
        Actual2,
        [Browsable(false)
        END
    }
    
    In the above example, only Actual and Actual2 would be displayed as a Radio-button list or Combobox.

--- Source/Examples/PropertyEditor/FeaturesDemo/SimpleObject.cs
+++ Source/Examples/PropertyEditor/FeaturesDemo/SimpleObject.cs
@@ -1,13 +1,30 @@
-﻿namespace FeaturesDemo
+﻿using System.ComponentModel;
+namespace FeaturesDemo
 {
-    public class SimpleObject 
-    {
-        public double Double { get; set; }
-        public int Int { get; set; }
-        public string String { get; set; }
-        public bool Bool { get; set; }
-        public Enum1 Enum { get; set; }
+	public class SimpleObject 
+	{
+		public double Double { get; set; }
+		public int Int { get; set; }
+		public string String { get; set; }
+		public bool Bool { get; set; }
+		public Enum1 Enum { get; set; }
+		public AllHidden1 AllHiddenEnum { get; set; }
 
-        public enum Enum1 { Value1, Value2, Value3 }
-    }
+		public enum Enum1 { 
+			[Browsable(false)]
+			NotShownValue, Value1, Value2,
+			[Browsable( false )]
+			[Description( "Description of AnotherNotShownValue" )]
+			AnotherNotShownValue,
+			[Description("Description of Value3")]
+			Value3 }
+
+		public enum AllHidden1
+		{
+			[Browsable( false )]
+			NotShownValue,
+			[Browsable( false )]
+			AnotherNotShownValue
+		}
+	}
 }
--- Source/PropertyTools.Wpf/Converters/EnumDescriptionConverter.cs
+++ Source/PropertyTools.Wpf/Converters/EnumDescriptionConverter.cs
@@ -4,50 +4,90 @@ using System.Linq;
 using System.ComponentModel;
 using System.Reflection;
 using System.Windows.Data;
+using System.Collections;
 
 namespace PropertyTools.Wpf
 {
-    /// <summary>
-    /// The EnumDescriptionConverter gets the Description attribute for Enum values.
-    /// </summary>
-    [ValueConversion(typeof(object), typeof(string))]
-    public class EnumDescriptionConverter : IValueConverter
-    {
-        #region IValueConverter Members
+	/// <summary>
+	/// The EnumDescriptionConverter gets the Description attribute for Enum values.
+	/// </summary>
+	[ValueConversion(typeof(object), typeof(string))]
+	public class EnumDescriptionConverter : IValueConverter
+	{
+		#region IValueConverter Members
 
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            // Default, non-converted result.
-            string result = value.ToString();
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			// Default, non-converted result.
+			string result = value.ToString();
 
-            var field = value.GetType().GetFields(BindingFlags.Static | BindingFlags.GetField | BindingFlags.Public).FirstOrDefault(f => f.GetValue(null).Equals(value));
+			var field = value.GetType().GetFields(BindingFlags.Static | BindingFlags.GetField | BindingFlags.Public).FirstOrDefault(f => f.GetValue(null).Equals(value));
 
-            if (field != null)
-            {
-                var descriptionAttribute = field.GetCustomAttributes<DescriptionAttribute>(true).FirstOrDefault();
-                if (descriptionAttribute != null)
-                {
-                    // Found the attribute, assign description
-                    result = descriptionAttribute.Description;
-                }
-            }
+			if (field != null)
+			{
+				var descriptionAttribute = field.GetCustomAttributes<DescriptionAttribute>(true).FirstOrDefault();
+				if (descriptionAttribute != null)
+				{
+					// Found the attribute, assign description
+					result = descriptionAttribute.Description;
+				}
+			}
 
-            return result;
-        }
+			return result;
+		}
 
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotSupportedException();
-        }
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 
-    public static class ReflectionExtensions
-    {
-        public static IEnumerable<T> GetCustomAttributes<T>(this FieldInfo fieldInfo, bool inherit)
-        {
-            return fieldInfo.GetCustomAttributes(typeof(T), inherit).Cast<T>();
-        }
-    }
+	public static class ReflectionExtensions
+	{
+		public static IEnumerable<T> GetCustomAttributes<T>(this FieldInfo fieldInfo, bool inherit)
+		{
+			return fieldInfo.GetCustomAttributes(typeof(T), inherit).Cast<T>();
+		}
+
+		/// <summary>
+		/// Filters on the browsable attribute.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="arr">The arr.</param>
+		/// <returns></returns>
+		public static List<object> FilterOnBrowsableAttribute<T>( this T arr ) where T : IEnumerable
+		{
+			// Default empty list
+			List<object> res = new List<object>();
+
+			// Loop each item in the enumerable
+			foreach( var o in arr ) {
+				// Get the field information for the current field
+				FieldInfo field = o.GetType().GetField( o.ToString(), BindingFlags.Static | BindingFlags.GetField | BindingFlags.Public );
+				if( field != null ) {
+					// Get the Browsable attribute, if it is declared for this field
+					var browsable = field.GetCustomAttributes<BrowsableAttribute>( true ).FirstOrDefault();
+					if( browsable != null ) {
+						// It is declared, is it true or false?
+						if( browsable.Browsable ) {
+							// Is true - field is not hidden so add it to the result.
+							res.Add( o );
+						}
+					}
+					else {
+						// Not declared so add it to the result
+						res.Add( o );
+					}
+				}
+				else {
+					// Can't evaluate, include it.
+					res.Add( o );
+				}
+			}
+
+			return res;
+		}
+	}
 }

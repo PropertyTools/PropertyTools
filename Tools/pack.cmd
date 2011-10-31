965900b3d7b13cdde963f52353bddde2f16c04b0
objo <objo@localhost>
2011-10-31 22:56:13 +0000

    Tools: corrected NuGet pack script

--- Tools/pack.cmd
+++ Tools/pack.cmd
@@ -1,5 +1,5 @@
 cd ..\Packages
-copy ..\source\PropertyTools.Wpf\bin\Release\*.dll PropertyTools.Wpf\lib
-copy ..\source\PropertyTools.Wpf\bin\Release\*.XML PropertyTools.Wpf\lib
+copy ..\Output\PropertyTools.Wpf.dll PropertyTools.Wpf\lib
+copy ..\Output\PropertyTools.Wpf.XML PropertyTools.Wpf\lib
 ..\tools\nuget.exe pack PropertyTools.Wpf\PropertyTools.Wpf.nuspec
 cd ..\Tools

d7fa9a2b7b216efdf936a38fafac5187249af0f8
objo <objo@localhost>
2011-11-12 16:22:12 +0000

    Tools: added OpenCover script, changed nuget scripts

--- Tools/pack.cmd
+++ Tools/pack.cmd
@@ -1,5 +1,4 @@
-cd ..\Packages
-copy ..\Output\PropertyTools.Wpf.dll PropertyTools.Wpf\lib
-copy ..\Output\PropertyTools.Wpf.XML PropertyTools.Wpf\lib
-..\tools\nuget.exe pack PropertyTools.Wpf\PropertyTools.Wpf.nuspec
-cd ..\Tools
+
+copy ..\Output\PropertyTools.Wpf.dll ..\Packages\PropertyTools.Wpf\lib
+copy ..\Output\PropertyTools.Wpf.XML ..\Packages\PropertyTools.Wpf\lib
+nuget.exe pack ..\Packages\PropertyTools.Wpf\PropertyTools.Wpf.nuspec -OutputDirectory ..\Packages
--- Tools/push.cmd
+++ Tools/push.cmd
@@ -1,3 +1,1 @@
-cd ..\Packages
-..\tools\nuget.exe push -source http://packages.nuget.org/v1/ %1 %NUGET_API_KEY%
-pause
+nuget.exe push -source http://packages.nuget.org/v1/ ..\Packages\PropertyTools.2011.11.2.nupkg %NUGET_ACCESS_KEY%

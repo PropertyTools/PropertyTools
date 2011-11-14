78dcea6bafc8164b45d9253cc8a1cba9c9d248d9
objo <objo@localhost>
2011-11-14 21:52:50 +0000

    Added PropertyTools.dll to the nuget package.

--- Tools/pack.cmd
+++ Tools/pack.cmd
@@ -1,4 +1,6 @@
 
+copy ..\Output\PropertyTools.dll ..\Packages\PropertyTools.Wpf\lib
+copy ..\Output\PropertyTools.XML ..\Packages\PropertyTools.Wpf\lib
 copy ..\Output\PropertyTools.Wpf.dll ..\Packages\PropertyTools.Wpf\lib
 copy ..\Output\PropertyTools.Wpf.XML ..\Packages\PropertyTools.Wpf\lib
 nuget.exe pack ..\Packages\PropertyTools.Wpf\PropertyTools.Wpf.nuspec -OutputDirectory ..\Packages > pack.log

967529d4701d501e58620473dbcaaefea6abbbbf
Oystein Bjorke <oystein.bjorke@gmail.com>
2012-01-13 20:53:42 +0000

    Changed version number to 2012.1.1

--- Source/Examples/PropertyEditor/CustomTypeDescriptorDemo/Properties/AssemblyInfo.cs
+++ Source/Examples/PropertyEditor/CustomTypeDescriptorDemo/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2011.10.1")]
-[assembly: AssemblyVersion("2011.10.1")]
-[assembly: AssemblyFileVersion("2011.10.1")]
+// [assembly: AssemblyVersion("2012.1.1")]
+[assembly: AssemblyVersion("2012.1.1")]
+[assembly: AssemblyFileVersion("2012.1.1")]
--- Source/Examples/TestLibrary/Properties/AssemblyInfo.cs
+++ Source/Examples/TestLibrary/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TestLibrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("PropertyTools")]
 [assembly: AssemblyProduct("TestLibrary")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("PropertyTools")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("2012.1.1")]
+[assembly: AssemblyVersion("2012.1.1")]
+[assembly: AssemblyFileVersion("2012.1.1")]

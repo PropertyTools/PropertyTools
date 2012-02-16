12b117f6d7def907e769ce4f5b9dd470cc3b0f4b
Oystein Bjorke <oystein.bjorke@gmail.com>
2012-02-16 06:18:17 +0100

    TestLibrary: improved HeaderPlacement test

--- Source/Examples/TestLibrary/Tests/TestHeaderPlacementAttribute.cs
+++ Source/Examples/TestLibrary/Tests/TestHeaderPlacementAttribute.cs
@@ -1,11 +1,22 @@
 namespace TestLibrary
 {
+    using System.ComponentModel;
+
     using PropertyTools.DataAnnotations;
 
     public class TestHeaderPlacementAttribute : TestBase
     {
+        [Category("HeaderPlacement.Above")]
         [HeaderPlacement(HeaderPlacement.Above), Height(100)]
-        public string Comment { get; set; }
+        public string HeaderAbove { get; set; }
+
+        [Category("HeaderPlacement.Collapsed")]
+        [HeaderPlacement(HeaderPlacement.Collapsed), Height(100)]
+        public string HeaderCollapsed { get; set; }
+
+        [Category("HeaderPlacement.Left")]
+        [HeaderPlacement(HeaderPlacement.Left), Height(100)]
+        public string HeaderLeft { get; set; }
 
         public override string ToString()
         {

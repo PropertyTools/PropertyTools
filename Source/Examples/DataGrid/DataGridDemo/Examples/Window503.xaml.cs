d321b00450e0b9a182cb72ae743d0767c80139a2
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-04-10 14:35:08 +0200

    DataGrid examples: use a static ItemsSource to make it possible to open two windows and check that they are in sync

--- Source/Examples/DataGrid/DataGridDemo/Examples/Window503.xaml.cs
+++ Source/Examples/DataGrid/DataGridDemo/Examples/Window503.xaml.cs
@@ -24,7 +24,7 @@
 //   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // </copyright>
 // <summary>
-//   Interaction logic for Window503.xaml
+//   Interaction logic for Window503.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -33,51 +33,46 @@ namespace DataGridDemo
     using System.Collections.ObjectModel;
 
     /// <summary>
-    /// Interaction logic for Window503.xaml
+    /// Interaction logic for Window503.
     /// </summary>
     public partial class Window503
     {
+        /// <summary>
+        /// The shared items source. This makes it possible to open two windows and verify that property changes are working!
+        /// </summary>
+        private static readonly ObservableCollection<ObservableCollection<bool>> StaticItemsSource;
+
+        /// <summary>
+        /// Initializes static members of the <see cref="Window503"/> class.
+        /// </summary>
+        static Window503()
+        {
+            StaticItemsSource = new ObservableCollection<ObservableCollection<bool>>
+                                {
+                                    new ObservableCollection<bool> { true, false, true },
+                                    new ObservableCollection<bool> { false, true, false },
+                                    new ObservableCollection<bool> { true, true, true }
+                                };
+        }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="Window503" /> class.
         /// </summary>
         public Window503()
         {
             this.InitializeComponent();
-
-            this.ItemsSource = new ObservableCollection<ObservableCollection<bool>>
-                                {
-                                    new ObservableCollection<bool>
-                                        {
-                                            true,
-                                            false,
-                                            true,
-                                        },
-                                    new ObservableCollection<bool>
-                                        {
-                                            false,
-                                            false,
-                                            true,
-                                        },
-                                    new ObservableCollection<bool>
-                                        {
-                                            true,
-                                            true,
-                                            false
-                                        },
-                                    new ObservableCollection<bool>
-                                        {
-                                            true,
-                                            false,
-                                            false
-                                        },
-                                };
-
             this.DataContext = this;
         }
 
         /// <summary>
-        /// Gets or sets the items.
+        /// Gets the items source.
         /// </summary>
-        public ObservableCollection<ObservableCollection<bool>> ItemsSource { get; set; }
+        public ObservableCollection<ObservableCollection<bool>> ItemsSource
+        {
+            get
+            {
+                return StaticItemsSource;
+            }
+        }
     }
 }

ec056253b69b97900ee868fc6799596b7ce84377
ZHZG <Zong.Gui.Zhu@dnvgl.com>
2015-06-17 09:12:45 +0800

    Fix build error.

--- Source/PropertyTools.Wpf/DataGrid/ControlFactory/DataGridControlFactory.cs
+++ Source/PropertyTools.Wpf/DataGrid/ControlFactory/DataGridControlFactory.cs
@@ -9,12 +9,15 @@
 
 namespace PropertyTools.Wpf
 {
+    using System.Linq;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Controls.Primitives;
     using System.Windows.Data;
     using System.Windows.Media;
     using System.Windows.Shapes;
+    
+    using PropertyTools.DataAnnotations;
 
     /// <summary>
     /// Creates display and edit controls for the DataGrid.

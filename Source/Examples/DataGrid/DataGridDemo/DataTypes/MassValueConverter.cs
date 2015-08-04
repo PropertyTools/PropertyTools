b756b4368105bcc4e32f40b37b865b3f08105680
Oystein Bjorke <oystein.bjorke@dnvgl.com>
2015-08-04 15:43:50 +0200

    Show usage of column converter in DataGridDemo

---
+++ Source/Examples/DataGrid/DataGridDemo/DataTypes/MassValueConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace DataGridDemo
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class MassValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var v = (Mass)value;
+            return v.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var s = value as string;
+            return Mass.Parse(s, culture);
+        }
+    }
+}

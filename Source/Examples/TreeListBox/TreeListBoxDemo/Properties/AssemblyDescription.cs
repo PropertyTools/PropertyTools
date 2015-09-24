5e7e20505a7ac8470fa8d5411796acacde1cea09
Oystein Bjorke <oystein.bjorke@gmail.com>
2015-09-24 09:09:46 +0200

    Rename GlobalAssemblyInfo to AssemblyInfo

---
+++ Source/Examples/TreeListBox/TreeListBoxDemo/Properties/AssemblyDescription.cs
@@ -0,0 +1,13 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyDescription.cs" company="PropertyTools">
+//   Copyright (c) 2014 PropertyTools contributors
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Windows;
+
+[assembly: AssemblyTitle("TreeListBox demo")]
+[assembly: AssemblyDescription("")]
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
---
+++ Source/PropertyTools.Wpf/Properties/AssemblyDescription.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyDescription.cs" company="PropertyTools">
+//   Copyright (c) 2014 PropertyTools contributors
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: AssemblyTitle("PropertyTools for WPF")]
+[assembly: AssemblyDescription("")]
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+
+[assembly: XmlnsPrefix("http://propertytools.org/wpf", "pt")]
+[assembly: XmlnsDefinition("http://propertytools.org/wpf", "PropertyTools.Wpf")]

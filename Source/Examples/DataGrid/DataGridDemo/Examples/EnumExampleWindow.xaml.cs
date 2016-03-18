8ad0d6f87838d4c989d1eb268a775cc261474df0
Oystein Bjorke <oystein.bjorke@gmail.com>
2016-03-18 22:16:08 +0100

    Add DataGrid example for the nullable enum bug #97

---
+++ Source/Examples/DataGrid/DataGridDemo/Examples/EnumExampleWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿namespace DataGridDemo
+{
+    using System.Collections.ObjectModel;
+
+    using PropertyTools;
+
+    public partial class EnumExampleWindow
+    {
+        public EnumExampleWindow()
+        {
+            this.InitializeComponent();
+            this.ItemsSource = new ObservableCollection<EnumObject> { new EnumObject() };
+            this.DataContext = this;
+        }
+
+        public ObservableCollection<EnumObject> ItemsSource { get; }
+
+        public class EnumObject : Observable
+        {
+            private Fruit fruit;
+
+            private Fruit? nullableFruit;
+
+            public Fruit Fruit
+            {
+                get
+                {
+                    return this.fruit;
+                }
+
+                set
+                {
+                    this.SetValue(ref this.fruit, value, "Fruit");
+                }
+            }
+
+            public Fruit? NullableFruit
+            {
+                get
+                {
+                    return this.nullableFruit;
+                }
+
+                set
+                {
+                    this.SetValue(ref this.nullableFruit, value, "NullableFruit");
+                }
+            }
+        }
+    }
+}

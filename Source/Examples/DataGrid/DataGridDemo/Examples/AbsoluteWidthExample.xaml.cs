f96c82418b4f172ae02dfeb471226e0cc012f522
Oystein Bjorke <oystein.bjorke@gmail.com>
2016-10-12 15:48:34 +0200

    Add examples

---
+++ Source/Examples/DataGrid/DataGridDemo/Examples/AbsoluteWidthExample.xaml.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AbsoluteWidthExample.xaml.cs" company="PropertyTools">
+//   Copyright (c) 2014 PropertyTools contributors
+// </copyright>
+// <summary>
+//   Interaction logic for AbsoluteWidthExample.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DataGridDemo
+{
+    /// <summary>
+    /// Interaction logic for AbsoluteWidthExample.
+    /// </summary>
+    public partial class AbsoluteWidthExample
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AbsoluteWidthExample" /> class.
+        /// </summary>
+        public AbsoluteWidthExample()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ Source/Examples/DataGrid/DataGridDemo/Examples/AutoWidthExample.xaml.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AutoWidthExample.xaml.cs" company="PropertyTools">
+//   Copyright (c) 2014 PropertyTools contributors
+// </copyright>
+// <summary>
+//   Interaction logic for GettingStartedExample.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DataGridDemo
+{
+    /// <summary>
+    /// Interaction logic for AutoWidthExample.
+    /// </summary>
+    public partial class AutoWidthExample
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutoWidthExample" /> class.
+        /// </summary>
+        public AutoWidthExample()
+        {
+            this.InitializeComponent();
+        }
+    }
+}

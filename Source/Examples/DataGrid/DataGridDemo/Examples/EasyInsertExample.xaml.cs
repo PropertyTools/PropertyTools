a812939d45f33acc8313724156523b2739aa9970
Oystein Bjorke <oystein.bjorke@gmail.com>
2016-10-24 11:32:05 +0200

    Add EasyInsert example

---
+++ Source/Examples/DataGrid/DataGridDemo/Examples/EasyInsertExample.xaml.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EasyInsertExample.xaml.cs" company="PropertyTools">
+//   Copyright (c) 2014 PropertyTools contributors
+// </copyright>
+// <summary>
+//   Interaction logic for EasyInsertExample.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DataGridDemo
+{
+    /// <summary>
+    /// Interaction logic for EasyInsertExample.
+    /// </summary>
+    public partial class EasyInsertExample
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EasyInsertExample" /> class.
+        /// </summary>
+        public EasyInsertExample()
+        {
+            this.InitializeComponent();
+        }
+    }
+}

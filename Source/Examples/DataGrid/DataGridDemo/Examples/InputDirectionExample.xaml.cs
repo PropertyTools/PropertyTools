6c8b959f67947d23be7bdcc7a50a1844a9a93015
Oystein Bjorke <oystein.bjorke@gmail.com>
2016-10-31 16:18:06 +0100

    add input direction example

---
+++ Source/Examples/DataGrid/DataGridDemo/Examples/InputDirectionExample.xaml.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InputDirectionExample.xaml.cs" company="PropertyTools">
+//   Copyright (c) 2014 PropertyTools contributors
+// </copyright>
+// <summary>
+//   Interaction logic for InputDirectionExample.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DataGridDemo
+{
+    /// <summary>
+    /// Interaction logic for InputDirectionExample.
+    /// </summary>
+    public partial class InputDirectionExample
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InputDirectionExample" /> class.
+        /// </summary>
+        public InputDirectionExample()
+        {
+            this.InitializeComponent();
+        }
+    }
+}

8a99c511267a12d91683c103820f20a75177d735
dtaylor-530 <d.taylor530@gmail.com>
2021-07-05 18:13:20 +0100

    Reverts changes to SimpleDemo. Move prior changes in SimpleDemo to SizeConfigurationDemo. Adds FontSize SpinControl to SizeConfigurationDemo demo to control Font Size.

---
+++ Source/Examples/PropertyGrid/SizeConfigurationDemo/Person.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Person.cs" company="PropertyTools">
+//   Copyright (c) 2014 PropertyTools contributors
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Windows.Media;
+
+namespace SizeConfigurationDemo
+{
+    using System.ComponentModel;
+
+    public enum Genders
+    {
+        Male,
+        Female
+    }
+
+    public class Person
+    {
+        [Category("Data")]
+        [DisplayName("Given name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Family name")]
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public double Height { get; set; }
+
+        public Genders Gender { get; set; }
+
+        public Color HairColor { get; set; }
+
+        [Description("Check the box if the person owns a bicycle.")]
+        public bool OwnsBicycle { get; set; }
+    }
+}
---
+++ Source/Examples/PropertyGrid/SizeConfigurationDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="PropertyTools">
+//   Copyright (c) 2014 PropertyTools contributors
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Windows;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]

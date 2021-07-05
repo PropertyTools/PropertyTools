3a8f5e5521c224ee692a23013d782b4c45efc12e
dtaylor-530 <d.taylor530@gmail.com>
2021-07-05 18:53:00 +0100

    Removes SizeConfigurationDemo

--- Source/Examples/PropertyGrid/SizeConfigurationDemo/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs" company="PropertyTools">
-//   Copyright (c) 2014 PropertyTools contributors
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-
-using System.Windows;
-
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]

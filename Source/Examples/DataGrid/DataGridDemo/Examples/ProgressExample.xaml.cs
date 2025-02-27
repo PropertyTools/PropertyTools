a45f844c2f3701809820e0d6523777173bdc2100
Oystein Bjorke <Oystein.Bjorke@dnvgl.com>
2025-02-27 17:14:34 +0100

    Add DataGrid progress bar example #385

---
+++ Source/Examples/DataGrid/DataGridDemo/Examples/ProgressExample.xaml.cs
@@ -0,0 +1,190 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TemplateExample.xaml.cs" company="PropertyTools">
+//   Copyright (c) 2014 PropertyTools contributors
+// </copyright>
+// <summary>
+//   Interaction logic for ProgressExample.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using PropertyTools;
+using PropertyTools.Wpf;
+
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace DataGridDemo
+{
+    /// <summary>
+    /// Interaction logic for ProgressExample.
+    /// </summary>
+    public partial class ProgressExample
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProgressExample" /> class.
+        /// </summary>
+        public ProgressExample()
+        {
+            this.InitializeComponent();
+        }
+    }
+
+    public class ProgressExampleViewModel
+    {
+        private DispatcherTimer timer;
+
+        public ICommand StartCommand { get; }
+        public ICommand StopCommand { get; }
+        public ICommand AddCommand { get; }
+
+        public ObservableCollection<ProgressItem> Items { get; set; } = new ObservableCollection<ProgressItem> {
+            new ProgressItem() { Progress = 0} ,
+            new ProgressItem() { Progress = 0.1} ,
+            new ProgressItem() { Progress = 0.5} ,
+            new ProgressItem() { Progress = 0.9} ,
+            new ProgressItem() { Progress = 1 } ,
+        };
+
+        public ProgressControlFactory ControlFactory { get; } = new ProgressControlFactory();
+        public ProgressCellDefinitionFactory CellDefinitionFactory { get; } = new ProgressCellDefinitionFactory();
+
+        public ProgressExampleViewModel()
+        {
+            this.StartCommand = new DelegateCommand(this.Start, () => this.timer == null);
+            this.StopCommand = new DelegateCommand(this.Stop, () => this.timer != null);
+            this.AddCommand = new DelegateCommand(() => this.Add(10));
+        }
+
+        public void Add(int n)
+        {
+            var r = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                this.Items.Add(new ProgressItem() { Progress = r.NextDouble() });
+            }
+        }
+
+        public void Start()
+        {
+            if (this.timer != null)
+            {
+                return;
+            }
+
+            this.timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(0.3);
+            timer.Tick += (s, e) =>
+            {
+                foreach (var item in this.Items)
+                {
+                    item.Progress = Math.Min(1, item.Progress + 0.01);
+                }
+            };
+
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            if (this.timer == null)
+            {
+                return;
+            }
+
+            this.timer.Stop();
+            this.timer = null;
+        }
+
+        public class ProgressCellDefinitionFactory : CellDefinitionFactory
+        {
+            public ProgressCellDefinitionFactory()
+            {
+            }
+
+            protected override CellDefinition CreateCellDefinitionOverride(CellDescriptor d)
+            {
+                if (d.Attributes.OfType<ProgressAttribute>().Any())
+                {
+                    return new ProgressCellDefinition();
+                }
+
+                return base.CreateCellDefinitionOverride(d);
+            }
+        }
+
+        public class ProgressControlFactory : DataGridControlFactory
+        {
+            protected override FrameworkElement CreateDisplayControlOverride(CellDefinition d)
+            {
+                if (d is ProgressCellDefinition)
+                {
+                    return CreateProgressControl(d);
+                }
+
+                return base.CreateDisplayControlOverride(d);
+            }
+
+            protected virtual FrameworkElement CreateProgressControl(CellDefinition d)
+            {
+                var c = new ProgressBar
+                {
+                    Minimum = 0,
+                    Maximum = 1
+                };
+                c.SetBinding(ProgressBar.ValueProperty, this.CreateBinding(d));
+                return c;
+            }
+        }
+
+        public class ProgressCellDefinition : CellDefinition
+        {
+        }
+
+        public class ProgressItem : Observable
+        {
+            private double progress;
+            private string status;
+
+            [Progress]
+            public double Progress
+            {
+                get => this.progress;
+                internal set
+                {
+                    if (this.SetValue(ref this.progress, value))
+                    {
+                        this.RaisePropertyChanged(nameof(ProgressValue));
+                        this.Status = GetStatusText(this.progress);
+                    }
+                }
+            }
+
+            public double ProgressValue { get => this.progress; set => this.Progress = value; }
+
+            public string Status
+            {
+                get => this.status;
+                set => this.SetValue(ref this.status, value);
+            }
+
+            private string GetStatusText(double progress)
+            {
+                if (progress >= 1) return "Done";
+                if (progress >= 0.9) return "Almost done";
+                if (progress >= 0.1) return "In progress";
+                if (progress > 0) return "Just started";
+                return "Not started";
+            }
+        }
+
+        [AttributeUsage(AttributeTargets.Property)]
+        public class ProgressAttribute : Attribute
+        {
+        }
+    }
+}

55d30638c4371693d91e7164ad2e852ef10b3ce4
Andrii Protskiv <aprotskiv@gmail.com>
2026-05-05 22:11:23 +0700

    added "instance" argument to match new method signature

--- Source/Examples/PropertyGrid/CustomFactoryDemo/Factories/CustomPropertyGridControlFactory.cs
+++ Source/Examples/PropertyGrid/CustomFactoryDemo/Factories/CustomPropertyGridControlFactory.cs
@@ -23,7 +23,8 @@ namespace CustomFactoryDemo
     /// </summary>
     public class CustomPropertyGridControlFactory : PropertyGridControlFactory
     {
-        public override FrameworkElement CreateControl(PropertyItem pi, PropertyControlFactoryOptions options)
+        public override FrameworkElement CreateControl(PropertyItem pi, PropertyControlFactoryOptions options, 
+            object instance)
         {
             // Check if the property is of type Range
             if (pi.Is(typeof(Range)))
@@ -32,7 +33,7 @@ namespace CustomFactoryDemo
                 return this.CreateRangeControl(pi, options);
             }
 
-            return base.CreateControl(pi, options);
+            return base.CreateControl(pi, options, instance);
         }
 
         protected virtual FrameworkElement CreateRangeControl(PropertyItem pi, PropertyControlFactoryOptions options)
--- Source/Examples/PropertyGrid/PropertyGridDemos/Examples/DataGridFactoryExample.xaml.cs
+++ Source/Examples/PropertyGrid/PropertyGridDemos/Examples/DataGridFactoryExample.xaml.cs
@@ -57,14 +57,15 @@ namespace PropertyGridDemos
 
     public class CustomControlFactory : PropertyGridControlFactory
     {
-        public override FrameworkElement CreateControl(PropertyItem pi, PropertyControlFactoryOptions options)
+        public override FrameworkElement CreateControl(PropertyItem pi, PropertyControlFactoryOptions options, 
+            object instance)
         {
             if (pi is BigCollectionPropertyItem)
             {
                 return this.CreateBigCollectionControl(pi, options);
             }
 
-            return base.CreateControl(pi, options);
+            return base.CreateControl(pi, options, instance);
         }
 
         protected virtual FrameworkElement CreateBigCollectionControl(PropertyItem property, PropertyControlFactoryOptions options)
